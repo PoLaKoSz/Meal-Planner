f893257e250435a15a7d449986ef6c685b8eb4c0
Tom PoLáKoSz <polakosz@freemail.hu>
2018-09-14 06:20:01 +0200

    sszem kezdem átveni fájlól sjlite-ba

---
+++ MealPlanner/DataAccessLayer/Migrations/009_Add_And_Remove_Column_Inside_Meal_Ingredient_Table.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace MealPlanner.DataAccessLayer.Migrations
+{
+    [Migration(201808161300)]
+    public class _009_Add_And_Remove_Column_Inside_Meal_Ingredient_Table : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("meal_ingredients")
+                .AddColumn("ingredient_id").AsInt64().NotNullable();
+
+            Rename.Column("mail_id")
+                .OnTable("meal_ingredients")
+                .To("meal_id");
+
+            Delete.Column("name").
+                FromTable("meal_ingredients");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("ingredient_id").
+                FromTable("meal_ingredients");
+
+            Rename.Column("meal_id")
+                .OnTable("meal_ingredients")
+                .To("mail_id");
+
+            Alter.Table("meal_ingredients")
+                .AddColumn("name").AsString();
+        }
+    }
+}
--- MealPlanner/DataAccessLayer/Tables/MealsTable.cs
+++ MealPlanner/DataAccessLayer/Tables/MealsTable.cs
@@ -1,8 +1,6 @@
 ﻿using Dapper;
 using MealPlanner.Models;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
